a4e65feb9a2012efdf1fa633296c41855d79262a
Zorbacoin <e.futura@virgilio.it>
2016-10-16 16:03:01 +0200

    Add files via upload

---
+++ src/qt/qtipcserver.h
@@ -0,0 +1,10 @@
+#ifndef QTIPCSERVER_H
+#define QTIPCSERVER_H
+
+// Define Zorbacoin-Qt message queue name
+#define BITCOINURI_QUEUE_NAME "ZorbacoinURI"
+
+void ipcScanRelay(int argc, char *argv[]);
+void ipcInit(int argc, char *argv[]);
+
+#endif // QTIPCSERVER_H
